1c13606832d95aed4339340d8f448bf5bdc0a192
Andrii Zatsepin <uzatsepin@gmail.com>
2023-11-21 16:53:10 +0200

    init of the project, ready left and right sidebar

---
+++ components/Sidebar/Right/index.vue
@@ -0,0 +1,64 @@
+<template>
+    <div class="flex flex-col">
+        <SidebarRightPreviewCard title="What's happening">
+            <SidebarRightPreviewCardItem v-for="whatsHappening in whatsHappeningItems">
+                <div>
+                    <h2 class="font-bold text-gray-800 text-md dark:text-white">{{ whatsHappening.title }}</h2>
+                    <p class="text-xs text-gray-400">{{ whatsHappening.count }}</p>
+                </div>
+            </SidebarRightPreviewCardItem>
+        </SidebarRightPreviewCard>
+        <SidebarRightPreviewCard title="Who to follow">
+            <SidebarRightPreviewCardItem v-for="whoToFollow in whoToFollowItems">
+                <div class="flex flex-row items-center justify-between p-2">
+                    <div class="flex flex-row">
+                        <img class="h-10 w-10 rounded-full" :src="whoToFollow.avatar" :alt="whoToFollow.name">
+                        <div class="flex flex-col ml-2">
+                            <h1 class="text-sm font-bold text-gray-900 dark:text-white">{{ whoToFollow.name }}</h1>
+                            <p class="text-xs text-gray-400">{{ whoToFollow.handle }}</p>
+                        </div>
+                    </div>
+                    <div class="flex h-full">
+                        <button
+                            class="px-4 py-2 font-bold text-xs text-white dark:text-black bg-black dark:bg-white rounded-full">Follow</button>
+                    </div>
+                </div>
+            </SidebarRightPreviewCardItem>
+
+        </SidebarRightPreviewCard>
+    </div>
+</template>
+<script setup>
+const whatsHappeningItems = ref([
+    {
+        title: 'SpaceX',
+        count: '18.8k Tweets'
+    },
+    {
+        title: '#CodingIsFun',
+        count: '8.8k Tweets'
+    },
+    {
+        title: '#art',
+        count: '2.1k Tweets'
+    },
+])
+const whoToFollowItems = ref([
+    {
+        name: 'Joe Baiden',
+        handle: '@JoeBiden',
+        avatar: 'https://picsum.photos/200/200'
+    },
+    {
+        name: 'Joe Baiden',
+        handle: '@JoeBiden',
+        avatar: 'https://picsum.photos/200/200'
+    },
+    {
+        name: 'Joe Baiden',
+        handle: '@JoeBiden',
+        avatar: 'https://picsum.photos/200/200'
+    }
+])
+
+</script>
---
+++ tailwind.config.js
@@ -0,0 +1,44 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [],
+  darkMode: 'class',
+  safelist: [
+    {
+        pattern: /text-(red|green|blue)-(100|400)/,
+        variants: ['group-hover']
+    },
+    {
+        pattern: /bg-(red|green|blue)-(100|400)/,
+        variants: ['group-hover']
+    }
+  ],
+  theme: {
+    extend: {
+        colors: {
+            dim: {
+                50: "#5F99F7",
+                100: "#5F99F7",
+                200: "#38444d",
+                300: "#202e3a",
+                400: "#253341",
+                500: "#5F99F7",
+                600: "#5F99F7",
+                700: "#192734",
+                800: "#162d40",
+                900: "#15202b",
+            },
+        }
+    },
+    screens: {
+        xs: '614px',
+        sm: '1002px',
+        md: '1022px',
+        lg: '1092px',
+        xl: '1280px',
+    }
+  },
+  plugins: [
+    // require('@tailwindcss/forms')
+  ],
+}
+
