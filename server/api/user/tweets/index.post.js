4c78cec19e902bad1d2b9034744ef716bfd72845
Andrii Zatsepin <uzatsepin@gmail.com>
2023-11-22 14:41:20 +0200

    feat: added backend for posting tweets and upload images

---
+++ server/api/user/tweets/index.post.js
@@ -0,0 +1,52 @@
+import formidable from 'formidable'
+import { createTweet } from '~/server/db/tweets'
+import { tweetTransformer } from '~/server/transformers/tweet'
+import { createMediaFile } from '~/server/db/mediaFiles'
+import { uploadToCloudinary } from '../../../utils/cloudinary.js'
+
+export default defineEventHandler(async (event) => {
+    const form = formidable({})
+
+    const response = await new Promise((resolve, reject) => {
+        form.parse(event.req, (err, fields, files) => {
+            if(err) {
+                reject(err)
+            }
+            resolve({fields, files})
+        })
+    })
+
+    const {fields, files} = response;
+
+    const userId = event.context?.auth?.user?.id;
+    const tweetData = {
+        authorId: userId,
+        text: fields.text[0],
+    }
+
+    const tweet = await createTweet(tweetData)
+
+    const filePromises = Object.keys(files).map(async key => {
+        
+        const file = files[key];
+
+        const filepath = file.map(file => {
+            return file.filepath
+        })
+
+        const cloudinaryResourse = await uploadToCloudinary(filepath[0])
+
+        return createMediaFile({
+            url: cloudinaryResourse.secure_url,
+            providerPublicId: cloudinaryResourse.public_id,
+            userId: userId,
+            tweetId: tweet.id
+        })
+    })
+    await Promise.all(filePromises)
+
+    
+    return {
+        tweet: tweetTransformer(tweet)
+    }
+})
